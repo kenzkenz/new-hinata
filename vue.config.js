eda8a47ece36d17fd2e49cba13e773967c6b2ea5
kenotiai <kenotiai@gmail.com>
2018-12-01 14:40:00 +0900

    設定ファイル修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
-//ビルドしたときパスを合わせる。
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? '/aaa/'
-        : ''
+  //  ビルドしたときのパス 空文字にして相対パスにする。
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? ''
+    : '',
+  // vue-awesomeの設定。最適化される？
+  transpileDependencies: [
+    /\bvue-awesome\b/
+  ],
+  // performance: {
+  //     // デフォルトは244 KiB. 10メガに変更
+  //   maxEntrypointSize: 10000000,
+  //   maxAssetSize: 10000000
+  // }
 }
